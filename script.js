cb8eabd9364ff1e39b8c9776745bd46775ff7712
Jacinth Boggess <jacinth@boggess.net>
2025-10-03 19:57:19 -0500

    Removed pictures from carousel and fixed some carousel logic. Also, removed cute pictures - will attempt to find a way to make them work better responsively.

--- script.js
+++ script.js
@@ -27,40 +27,44 @@ function carousel(click = 0) {
     console.log("Carousel called");
     let slides = document.getElementsByClassName("mySlides");
 
-    // Hide all images
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.opacity = 0;
-        slides[i].style.display = "hidden";
+    if(slides.length > 1){
+        // Hide all images
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.opacity = 0;
+            slides[i].style.display = "hidden";
+        }
+
+        if (click === 0) {
+            // Auto mode
+            slideIndex++;
+            if (slideIndex >= slides.length) { 
+                slideIndex = 0; 
+            }
+        } else if (click === 1) {
+            // Forward click
+            slideIndex++;
+            if (slideIndex >= slides.length) { 
+                slideIndex = 0; 
+            }
+        } else {
+            // Backward click
+            slideIndex--;
+            if (slideIndex < 0) { 
+                slideIndex = slides.length - 1; 
+            }
+        }
+
+        // Show the correct image
+        slides[slideIndex].style.display = "block";
+        slides[slideIndex].style.opacity = 1;
+
+        // Auto-transition every 3 seconds if not manually clicked
+        if (click === 0) {
+            setTimeout(carousel, 4000);
+        }
     }
 
-    if (click === 0) {
-        // Auto mode
-        slideIndex++;
-        if (slideIndex >= slides.length) { 
-            slideIndex = 0; 
-        }
-    } else if (click === 1) {
-        // Forward click
-        slideIndex++;
-        if (slideIndex >= slides.length) { 
-            slideIndex = 0; 
-        }
-    } else {
-        // Backward click
-        slideIndex--;
-        if (slideIndex < 0) { 
-            slideIndex = slides.length - 1; 
-        }
-    }
-
-    // Show the correct image
-    slides[slideIndex].style.display = "block";
-    slides[slideIndex].style.opacity = 1;
-
-    // Auto-transition every 3 seconds if not manually clicked
-    if (click === 0) {
-        setTimeout(carousel, 4000);
-    }
+    
 }
 
 function ensureValidity(checkboxChoices){
