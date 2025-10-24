f20dc30656c370f4520e3dec2a751d99c5b59100
Jacinth Boggess <jacinth@boggess.net>
2025-10-23 22:11:33 -0500

    Attempting to speedup load time

--- scripts/load-data.js
+++ scripts/load-data.js
@@ -1,4 +1,4 @@
-fetch("data.json")
+fetch("./data.json")
 .then(res => res.json())
 .then((contents) => {
     let data = JSON.parse(contents["data"]);
@@ -57,58 +57,39 @@ fetch("data.json")
     });
 
     function createTestimonial(quote, quote_provider){
-        const testimonials_section = document.getElementById("Testimonials");
-        const testimonials_list = testimonials_section.querySelector(".testimonial-list");
-
-        let testimony = document.createElement("div");
-        testimony.className = "testimony";
-
-        // Content (Quote)
-        let content = document.createElement("div");
-        let leftQuote = document.createElement("div");
-        let testimony_section = document.createElement("div");
-        let rightQuote = document.createElement("div");
-
-        leftQuote.className = "leftQuote";
-        testimony_section.className = "testimony-section";
-        rightQuote.className = "rightQuote";
-
-        let quote_img_l = document.createElement("img");
-        quote_img_l.src = "pictures/leftQ.svg";
-
-        let quote_img_r = document.createElement("img");
-        quote_img_r.src = "pictures/leftQ.svg";
-
-        leftQuote.appendChild(quote_img_l);
-        rightQuote.appendChild(quote_img_r);
-
-        content.appendChild(leftQuote);
-        testimony_section.innerText = quote;
-        content.appendChild(testimony_section);
-        content.appendChild(rightQuote);
-
-        // Contributor (Person who contributed the testimonial)
-        let contributor = document.createElement("div");
-        contributor.className = "contributor";
-        let personQuoted = quote_provider != "" ? quote_provider : "Unknown";
-        contributor.innerText = "-" + personQuoted;
-
-        testimony.appendChild(content);
-        testimony.appendChild(contributor);
-
-        testimonials_list.appendChild(testimony);
+        return `
+            <div class="testimony">
+                <div>
+                    <div class="leftQuote">
+                        <img src="./pictures/leftQ.svg">
+                    </div>
+                    <div class="testimony-section">
+                        ${quote}
+                    </div>
+                    <div class="rightQuote">
+                        <img src="./pictures/leftQ.svg">
+                    </div>
+                </div>
+                <div class="contributor">${quote_provider}</div>
+            </div>
+        `;
     }
+
+    let allTestimonials = "";
+    let allNavs = "";
+    let numberOfTestimonials = testimonials_authors.length;
+    const Testimonials = document.getElementById("Testimonials");
+    const Testimonials_list = Testimonials.querySelector(".testimonial-list");
+    const Testimonials_nav = Testimonials.querySelector(".nav");
+
 
     for(let i = 0; i < testimonials_quotes.length; i++){
-        createTestimonial(testimonials_quotes[i], testimonials_authors[i]);
-        let testimonials_section = document.getElementById("Testimonials");
-        let testimonials_nav = testimonials_section.querySelector(".nav");
-
-        let nav_img = document.createElement("img");
-        nav_img.src = i == 0 ? "pictures/NavCircle_B.svg" : "pictures/NavCircle_W.svg";
-
-        testimonials_nav.appendChild(nav_img);
+        allTestimonials += createTestimonial(testimonials_quotes[i], testimonials_authors[i]);
+        allNavs += i == 0 ? '<img src="./pictures/NavCircle_B.svg">' : '<img src="./pictures/NavCircle_W.svg">';
     }
+
+    Testimonials_list.innerHTML = allTestimonials;
+    Testimonials_nav.innerHTML = allNavs;
 
     // Services
     let services = {};
@@ -148,119 +129,101 @@ fetch("data.json")
 
     if(Object.keys(image_icon_srcs).length == 0){
         image_icon_srcs = {
-            "Tidying Up": "pictures/Services/Organization.svg",
-            "Floors": "pictures/Services/Floors.svg",
-            "Kitchen": "pictures/Services/Kitchen.svg",
-            "Bathrooms": "pictures/Services/Bathroom.svg",
-            "Windows": "pictures/Services/Window.svg",
-            "Kitchen & Appliances": "pictures/Services/Kitchen.svg",
-            "Living Areas": "pictures/Services/Living_Areas.svg",
-            "Windows & Doors": "pictures/Services/Window.svg"
+            "Tidying Up": "./pictures/Services/Organization.svg",
+            "Floors": "./pictures/Services/Floors.svg",
+            "Kitchen": "./pictures/Services/Kitchen.svg",
+            "Bathrooms": "./pictures/Services/Bathroom.svg",
+            "Windows": "./pictures/Services/Window.svg",
+            "Kitchen & Appliances": "./pictures/Services/Kitchen.svg",
+            "Living Areas": "./pictures/Services/Living_Areas.svg",
+            "Windows & Doors": "./pictures/Services/Window.svg"
         };
     }  
 
-    function createServiceCard(allServices){
+    function createServiceCards(allServices){
         let services_section = document.getElementById("Services");
         let container = services_section.querySelector(".container");
 
-        Object.keys(allServices).forEach(service => {
-            let card = document.createElement("div");
-            card.className = "card";
+        const card_html = (title, services) => {
+            return `
+            <div class="card">
+                <div class="name">
+                    <h3>${title}</h3>
+                </div>
+                <div class="content">
+                    ${services}
+                </div>
+            </div>
+        `;
+        };
 
-            let serviceName = document.createElement("div");
-            serviceName.className = "name";
-            let heading = document.createElement("h3");
-            heading.innerText = service;            
-            serviceName.appendChild(heading);
-            if(includes[service] != undefined){
-                let includes_text = document.createElement("div");
-                console.log("INCLUDES:", includes);
-                includes_text.innerText = includes[service];
+        const services_html = (title, descriptors) => {
+            console.log("Services HMTL");
+            return `
+                <div class="service">
+                    <div class="title">
+                        <div>
+                            <img src="${image_icon_srcs[title] != undefined ? image_icon_srcs[title] : './pictures/Services/Organization.svg'}">
+                            <div class="circle"></div>
+                        </div>
+                        <span>${title}</span>
+                    </div>
+                    <ul>
+                        ${descriptors}
+                    </ul>
+                </div>
+            `;
+        }
 
-                serviceName.appendChild(includes_text);
+        const descriptors_html = (descriptors) => {
+            let allDescriptors = "";
+            for(let i = 0; i < descriptors.length; i++){
+                allDescriptors += "<li>"+descriptors[i]+"</li>";
             }
 
-            let content = document.createElement("div");
-            content.className = "content";
+            return allDescriptors;
+        }
 
+        let cards = "";
+
+        Object.keys(allServices).forEach(service => {
+            let services = "";
             Object.keys(allServices[service]).forEach(serviceName => {
-                let service_container = document.createElement("div");
-                service_container.className = "service";
-                // service_container.setAttribute('data-service', 'BC Sweeping & Dusting');
-
-                let title = document.createElement("div");
-                title.className = "title";
-
-                let icon_container = document.createElement("div");
-                let icon_img = document.createElement("img");
-                icon_img.src = image_icon_srcs[serviceName] != undefined ? image_icon_srcs[serviceName] : "pictures/Services/Organization.svg";
-                let circle = document.createElement("div");
-                circle.className = "circle";
-                icon_container.appendChild(icon_img);
-                icon_container.appendChild(circle);
-
-                let title_span = document.createElement("span");
-                title_span.innerText = serviceName;
-
-                title.appendChild(icon_container);
-                title.appendChild(title_span);
-
-                let descriptors = allServices[service][serviceName];
-                let ul = document.createElement("ul");
-
-                for(let i = 0; i < descriptors.length; i++){
-                    let li = document.createElement("li");
-                    li.innerText = descriptors[i];
-                    ul.appendChild(li);
-                }
-
-                service_container.appendChild(title);
-                service_container.appendChild(ul);
-
-                content.appendChild(service_container);
+                let description = descriptors_html(allServices[service][serviceName]);
+                services += services_html(serviceName, description);
             })
 
-            card.appendChild(serviceName);
-            card.appendChild(content);
-
-            container.appendChild(card);
+            cards += card_html(service, services);
         })
+
+        container.innerHTML = cards;
     }
 
-    createServiceCard(services);
+    createServiceCards(services);
 
     // Fully Insured
     if(general["Fully Insured"] != undefined){
-        let fullyInsuredText = document.createElement("span");
-        fullyInsuredText.innerText = general["Fully Insured"];
-        document.getElementById("FullyInsured").appendChild(fullyInsuredText);
+        document.getElementById("FullyInsured").innerHTML = `<span>${general["Fully Insured"]}</span>`;
     }
 
     // Promotion
     let promotion_section = document.getElementById("Promotion");
 
-    if(Object.keys(data["Promotions"]).length != 0){     
+    if(Object.keys(data["Promotions"]).length != 0){
+        let all_promotions = "";    
         data["Promotions"].forEach(promotion => {
-            let title_section = document.createElement("div");
-            let heading = document.createElement("h2");
-            heading.innerText = promotion["Title"];
-            let span = document.createElement("span");
-            span.innerText = "Valid from " + promotion["Start Date"] + " - " + promotion["End Date"];
-            title_section.appendChild(heading);
-            title_section.appendChild(span);
-
-            let description = document.createElement("div");
-            description.className = "p_description";
-            description.innerText = promotion["Content"];
-
-            promotion_section.appendChild(title_section);
-            promotion_section.appendChild(description);
+            all_promotions += `
+                <div>
+                    <h2>${promotion["Title"]}</h2>
+                    <span>Valid from ${promotion["Start Date"]} - ${promotion["End Date"]}</span>
+                </div>
+                <span class="p_description">${promotion["Content"]}</span>
+            `;
         });
 
-        let arrow_img = document.createElement("img");
-        arrow_img.src = "pictures/dArrow.svg";
+        all_promotions += `<img src="./pictures/dArrow.svg">`;
 
-        promotion_section.appendChild(arrow_img);
+        promotion_section.innerHTML = all_promotions;
     }else{
         promotion_section.style.display = "none";
     }
